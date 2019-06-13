270ba98584885767b7bd8cf1eb6463e1ecb1dec5
Carolina <carolinaevevarela@gmail.com>
2019-06-12 21:54:36 -0400

    Edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'todos/index'
   get 'todos/new'
   get 'todos', to: 'todos#show', as: 'todo'
+  get 'todos', to: 'todos#edit'
   post 'todos', to: 'todos#create'
   root 'todos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
